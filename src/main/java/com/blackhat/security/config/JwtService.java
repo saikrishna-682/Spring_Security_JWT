d0e7476692696049c6e8b32a947af424d4bde68c
saikrishna <ss682@njit.edu>
2023-08-25 14:03:59 -0400

    added all the nessessary methods for to access the token as well as the objects using filter

--- src/main/java/com/blackhat/security/config/JwtService.java
+++ src/main/java/com/blackhat/security/config/JwtService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -28,6 +29,11 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    // if just needed the user details
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
     public String generateToken(Map<String, Object> extraClaims,
                                 UserDetails userDetails){
         return Jwts
@@ -38,6 +44,19 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token){
